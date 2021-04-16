d22bd71df630f0d5b47ceb4492eb3442d3010e73
Seun Odeyemi <blacng@gmail.com>
2021-04-16 14:09:31 -0400

    Adding RESTful Routes

---
+++ src/tests/test_users.py
@@ -0,0 +1,80 @@
+# src/tests/test_users.py
+
+
+import json
+from src import db
+from src.api.models import User
+
+
+def test_add_user(test_app, test_database):
+    client = test_app.test_client()
+    resp = client.post(
+        '/users',
+        data=json.dumps({
+            'username': 'michael',
+            'email': 'michael@testdriven.io'
+        }),
+        content_type='application/json',
+    )
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == 201
+    assert 'michael@testdriven.io was added!' in data['message']
+
+
+def test_add_user_invalid_json(test_app, test_database):
+    client = test_app.test_client()
+    resp = client.post(
+        '/users',
+        data=json.dumps({}),
+        content_type='application/json',
+    )
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == 400
+    assert 'Input payload validation failed' in data['message']
+
+
+def test_add_user_invalid_json_keys(test_app, test_database):
+    client = test_app.test_client()
+    resp = client.post(
+        '/users',
+        data=json.dumps({"email": "john@testdriven.io"}),
+        content_type='application/json',
+    )
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == 400
+    assert 'Input payload validation failed' in data['message']
+
+
+def test_add_user_duplicate_email(test_app, test_database):
+    client = test_app.test_client()
+    client.post(
+        '/users',
+        data=json.dumps({
+            'username': 'michael',
+            'email': 'michael@testdriven.io'
+        }),
+        content_type='application/json',
+    )
+    resp = client.post(
+        '/users',
+        data=json.dumps({
+            'username': 'michael',
+            'email': 'michael@testdriven.io'
+        }),
+        content_type='application/json',
+    )
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == 400
+    assert 'Sorry. That email already exists.' in data['message']
+
+
+def test_single_user(test_app, test_database):
+    user = User(username='jeffrey', email='jeffrey@testdriven.io')
+    db.session.add(user)
+    db.session.commit()
+    client = test_app.test_client()
+    resp = client.get(f'/users/{user.id}')
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == 200
+    assert 'jeffrey' in data['username']
+    assert 'jeffrey@testdriven.io' in data['email']
