91d52af1c125abe7609170cd2bbc2cc6c51e7b17
Seun Odeyemi <blacng@gmail.com>
2021-05-01 01:47:23 -0400

    API Documentation

--- src/api/ping.py
+++ src/api/ping.py
@@ -1,11 +1,9 @@
 # src/api/ping.py
 
 
-from flask import Blueprint
-from flask_restx import Api, Resource
+from flask_restx import Namespace, Resource  # updated
 
-ping_blueprint = Blueprint("ping", __name__)
-api = Api(ping_blueprint)
+ping_namespace = Namespace("ping")  # new
 
 
 class Ping(Resource):
@@ -13,4 +11,4 @@ class Ping(Resource):
         return {"status": "success", "message": "pong"}
 
 
-api.add_resource(Ping, "/ping")
+ping_namespace.add_resource(Ping, "")  # updated
